c87167ffecc16fd8702b01e0a1bb5cf9735fd6b5
Ziga Povhe <ziga.povhe2@gmail.com>
2019-01-26 16:28:31 +0100

    Added onClickListener on a button and connected two activities by an intent. Also fixed some previous bugs.

--- app/src/main/java/si/povhe/swoosh/MainActivity.kt
+++ app/src/main/java/si/povhe/swoosh/MainActivity.kt
@@ -1,12 +1,19 @@
 package si.povhe.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getBtn.setOnClickListener {
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
