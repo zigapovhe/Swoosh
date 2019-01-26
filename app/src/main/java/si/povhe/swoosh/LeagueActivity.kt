0e3c99a5234843e95fd1af60934cd52354d952a5
Ziga Povhe <ziga.povhe2@gmail.com>
2019-01-26 17:44:49 +0100

    Added SkillActivity and connected some things together.

--- app/src/main/java/si/povhe/swoosh/LeagueActivity.kt
+++ app/src/main/java/si/povhe/swoosh/LeagueActivity.kt
@@ -1,12 +1,19 @@
 package si.povhe.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_league.*
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+
+        nextBtn.setOnClickListener {
+            val skillIntent = Intent(this, SkillActivity::class.java)
+            startActivity(skillIntent)
+        }
     }
 }
--- app/src/main/java/si/povhe/swoosh/MainActivity.kt
+++ app/src/main/java/si/povhe/swoosh/MainActivity.kt
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
